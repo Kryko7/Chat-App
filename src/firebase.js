25568477549f4000cd620fc6ed4f2ef351227a65
Kryko7 <61829993+Kryko7@users.noreply.github.com>
2023-09-11 10:58:29 +0530

    Added firebase.js
    Created a Firebase project and added the configuration.

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC22FjT577T4r41HiINJsFT5bi-dgGkgbo",
+  authDomain: "babble-ac34d.firebaseapp.com",
+  projectId: "babble-ac34d",
+  storageBucket: "babble-ac34d.appspot.com",
+  messagingSenderId: "777737466729",
+  appId: "1:777737466729:web:146bb31ffbc6ef855383f8"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
